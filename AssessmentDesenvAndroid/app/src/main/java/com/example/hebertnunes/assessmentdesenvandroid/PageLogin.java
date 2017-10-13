d52473e6887b919a6ea6e87266f1394b3836f3b5
Hebert Nunes <hebertcunha1@gmail.com>
2017-10-13 14:08:14 -0300

    Criando a trasicao para a view de login

--- AssessmentDesenvAndroid/app/src/main/java/com/example/hebertnunes/assessmentdesenvandroid/PageLogin.java
+++ AssessmentDesenvAndroid/app/src/main/java/com/example/hebertnunes/assessmentdesenvandroid/PageLogin.java
@@ -2,6 +2,7 @@ package com.example.hebertnunes.assessmentdesenvandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PageLogin extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class PageLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_login);
+
+
     }
 }
